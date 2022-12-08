fd943c71ba2dc7e929ab17befe54fc59c2a29c86
Kim-Do_Kyoung <0409kdk@gmail.com>
2022-12-08 18:50:05 +0900

    test입니다

---
+++ CAMPICK/src/com/campick/model/BoradDao.java
@@ -0,0 +1,74 @@
+package com.campick.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class BoradDao {
+	public static final int BORAD_INSERT_SUCCESS = 1;
+	private static BoradDao instance = new BoradDao();
+	
+	private BoradDao() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static BoradDao getInstance() {
+		return instance;
+	}
+	
+	private Connection getConnection() {
+		
+		Context context = null;
+		DataSource dataSource = null;
+		Connection connection = null;
+		try {
+			context = new InitialContext();
+			dataSource = (DataSource)context.lookup("java:comp/env/jdbc/Oracle11g");
+			connection = dataSource.getConnection();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	//게시판에 글 등록
+	public int insertBorad(BoradDto dto) {
+		int ri = 0;
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		//DB에 insert할 sql문
+		String sql = "insert into borad(borad_id,borad_visit,borad_suggestion,borad_date,camp_name,"
+				+ "borad_period_first,borad_period_second,borad_name,user_name,borad_text,borad_img)"
+				+ " values (borad_seq.nextval?,?,?,?,?,?,?,?,?,?)";
+		try {
+			connection = getConnection();
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, dto.getBorad_visit());
+			pstmt.setInt(2, dto.getBorad_suggestion());
+			pstmt.setString(3, dto.getBorad_date());
+			pstmt.setString(4, dto.getCamp_name());
+			pstmt.setString(5, dto.getBorad_period_first());
+			pstmt.setString(6, dto.getBorad_period_second());
+			pstmt.setString(7, dto.getBorad_name());
+			pstmt.setString(8, dto.getUser_name());
+			pstmt.setString(9, dto.getBorad_text());
+			pstmt.setString(10, dto.getBorad_img());
+			pstmt.executeUpdate();
+			ri = BORAD_INSERT_SUCCESS;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(connection != null) connection.close();
+			}catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return ri;
+	}
+	
+}
