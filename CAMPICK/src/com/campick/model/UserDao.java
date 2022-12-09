6fe9df480e38ef62de20ad8a85d4355c44e83c67
damni <damni@DESKTOP-032VV2C>
2022-12-09 17:53:09 +0900

    회원가입

--- CAMPICK/src/com/campick/model/UserDao.java
+++ CAMPICK/src/com/campick/model/UserDao.java
@@ -1,6 +1,75 @@
 package com.campick.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class UserDao {
+	
+	private static UserDao instance = new UserDao();
+	
+	public UserDao() {}
+	
+	public static UserDao getInstance() {
+		return instance;
+	}
+	
+	private Connection getConnection() {
+	      Context context = null;
+	      DataSource dataSource = null;
+	      Connection connection = null;
+	      
+	      
+	      try {
+	         context = new InitialContext();
+	         dataSource = (DataSource) context.lookup("java:/comp/env/jdbc/Oracle11g");
+	         connection = dataSource.getConnection();
+	         System.out.println("getConnection 되었습니다!!");
+	      } catch (Exception e) {
+	         System.out.println("getConnection()예외");
+	         e.printStackTrace();
+	      }
+	      return connection;
+	}
+	
+	public void register(UserDto user) {
+		System.out.println("dao까지 넘어옴~~");
+	      
+	      Connection conn = null;
+	      PreparedStatement pstmt = null;
+	      
+	      try {
+
+	          conn = getConnection();
+	          pstmt = conn.prepareStatement("INSERT INTO USER_TB VALUES(?, ?, ?, ?, ?, ?)");
+	          pstmt.setString(1, user.getId());
+	          pstmt.setString(2, user.getPw());
+	          pstmt.setString(3, user.getName());
+	          pstmt.setString(4, user.getPhone());
+	          pstmt.setString(5, user.getEmail());
+	          pstmt.setString(6, user.getAddr());
+	          
+	          pstmt.executeUpdate();
+	          
+	      
+	      }catch(SQLException e) {
+	    	  System.out.println("register 오류!!");
+	    	  e.printStackTrace();
+	      }finally {
+	    	  try {
+	    		  pstmt.close();
+	    		  conn.close();
+	    	  }catch(Exception e2) {
+	    		  e2.printStackTrace();
+	    	  }
+	      }
+		
+	}
+	
 	
 	
 }
--- CAMPICK/src/com/campick/service/UserService.java
+++ CAMPICK/src/com/campick/service/UserService.java
@@ -1,5 +1,10 @@
 package com.campick.service;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public interface UserService {
+	
+public void execute(HttpServletRequest request, HttpServletResponse response);
 
 }
