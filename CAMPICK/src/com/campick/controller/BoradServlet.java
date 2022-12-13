d3f8b43caaba060457bf9f3c49df25abb0ae7148
Kim-Do_Kyoung <0409kdk@gmail.com>
2022-12-13 09:01:40 +0900

    asd

--- CAMPICK/src/com/campick/controller/BoradServlet.java
+++ CAMPICK/src/com/campick/controller/BoradServlet.java
@@ -86,16 +86,6 @@ public class BoradServlet extends HttpServlet {
 			
 		//글쓰기 화면에서 db로 입력
 		}else if(action.equals("insert")){
-//			BoradDto dto = new BoradDto();
-//			dto.setBorad_name(request.getParameter("borad_name"));
-//			dto.setCamp_name(request.getParameter("camp_name"));
-//			dto.setBorad_period_first(request.getParameter("borad_period_first"));
-//			dto.setBorad_period_second(request.getParameter("borad_period_second"));
-//			dto.setBorad_text(request.getParameter("borad_text"));
-//			dto.setBorad_img(request.getParameter("borad_img"));
-//			
-//			request.setAttribute("dto", dto);
-			
 			String saveFolder = "C:\\Project_camp\\CAMPICK\\CAMPICK\\WebContent\\image";		//사진을 저장할 경로
 			String encType = "utf-8";				//변환형식
 			int maxSize=5*1024*1024;				//사진의 size
